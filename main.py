6c20695bb80b8eadf852a00fbf9cd2e39ee33b7f
Bruno <brunorimassa@gmail.com>
2021-08-03 18:12:48 -0400

    Update: -DataBase. PostgreSQL intead of SQLite - requirements.txt (DB)

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
-
 from sqlalchemy import ForeignKey
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
@@ -11,14 +10,21 @@ from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# Move the line above to the Config Var (Enviromental variables to the configuration Menu in Heroku):
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
-##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+##CONNECT TO DB:
+# LOCAL - SQLite:
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'  # This line was to use SQLite
+# HEROKU - PostgreSQL:
+# The next line is the configuration of Postgres SQL in Heroku:
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'sqlite:///blog.db')  # The "," is for when running Locally
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
